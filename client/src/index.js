840a5d9e2699b5d65f79fb72c551efa3dd7eaecf
touregeldi <tore.kabi@gmail.com>
2021-01-08 00:16:36 +0500

    aaaaaaa

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import { Provider } from 'react-redux'
 import configureStore from './store/configureStore'
 import { addHero, startAddHero } from './actions/heroes';
@@ -48,9 +48,3 @@ store.dispatch(startAddHero()).then(() => {
     document.getElementById('root')
   );
 })
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
